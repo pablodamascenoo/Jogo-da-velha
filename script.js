2097cb8b2b221ff9e17842309f202e28d458b362
pablodamascenoo <pablofacex@gmail.com>
2022-01-30 15:08:57 -0300

    adicionada a função clear_game

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 let last = ""
+let count = 0
 
 function change_square(obj){
 
     square = obj.querySelector(".object")
+
+
 
     if(!square.classList.contains("circle") && !square.classList.contains("cross")){
 
@@ -16,6 +19,30 @@ function change_square(obj){
             square.classList.add("circle")
             last = "circle" 
         }
+
+        count++
     }
 
+    
+}
+
+function clear_game(){
+    
+    if(count >=9){
+
+        let all = document.querySelectorAll(".object")
+
+        for (i in all){
+            if(all[i].classList.contains("circle")){
+                all[i].classList.remove("circle")
+            }
+            else if(all[i].classList.contains("cross")){
+                all[i].classList.remove("cross")
+            }
+            count--;
+            if(count == 0){
+                break
+            }
+        }
+    }
 }
